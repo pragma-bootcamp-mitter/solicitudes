19713311d115c39e74e58e8552dd5495c227cccf
Mauricio Rincón <causemao@gmail.com>
2025-09-02 22:35:01 -0500

    Revert "Registar solicitudes (#1)" (#4)
    This reverts commit 781a9d134656c9072b123be63fe06d9827a41e65.

--- domain/usecase/src/main/java/co/com/pragma/bootcamp/usecase/registrarsolicitud/helper/DomainConstants.java
+++
@@ -1,8 +0,0 @@
-package co.com.pragma.bootcamp.usecase.registrarsolicitud.helper;
-
-public final class DomainConstants {
-
-    private DomainConstants() {}
-
-    public static final String PENDING_REVIEW_STATE = "PENDING_REVIEW";
-}
